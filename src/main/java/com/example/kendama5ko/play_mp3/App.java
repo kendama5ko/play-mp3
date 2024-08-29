cf36b5c50ba122a44f1c4d32b6690e3a8efbfb95
kendama5ko <hukurahagix@yahoo.co.jp>
2024-08-29 15:36:22 +0900

    first commit

---
+++ src/main/java/com/example/kendama5ko/play_mp3/App.java
@@ -0,0 +1,13 @@
+package com.example.kendama5ko.play_mp3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
