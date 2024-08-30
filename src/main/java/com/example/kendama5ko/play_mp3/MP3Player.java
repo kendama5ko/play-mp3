94572604848f6494c275c5624035616c423b338a
kendama5ko <hukurahagix@yahoo.co.jp>
2024-08-30 09:32:09 +0900

    [機能追加] audioファイルを選択可能にした

--- src/main/java/com/example/kendama5ko/play_mp3/MP3Player.java
+++ src/main/java/com/example/kendama5ko/play_mp3/MP3Player.java
@@ -9,20 +9,41 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MP3Player extends Application {
+	
 	private MediaPlayer mediaPlayer;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("MP3 Player");
 
-        // ファイルパスを指定（実際のMP3ファイルのパスに変更してください）
-        String path = "Jazz Piano BGM.mp3";
-        Media media = new Media(new File(path).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
 
+
+        // ファイルを選択するボタンを作成
+        Button selectFileButton = new Button("MP3ファイルを選択");
+
+        // ファイルを選択するボタンが押されたときの動作を設定
+        selectFileButton.setOnAction(e -> {
+            FileChooser audioFileChooser = new FileChooser();
+            audioFileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio Files", "*.mp3", "*.wav", "*.m4a"));
+            File selectedAudioFile = audioFileChooser.showOpenDialog(primaryStage);
+            
+            if (selectedAudioFile != null) {
+                // メディアプレイヤーがすでに存在する場合は停止してリセット
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer.dispose();
+                }
+                // 新しいファイルを再生
+                Media media = new Media(selectedAudioFile.toURI().toString());
+                mediaPlayer = new MediaPlayer(media);
+                mediaPlayer.play();
+            }
+        });
+            
         Button playButton = new Button("Play");
         playButton.setOnAction(e -> mediaPlayer.play());
 
@@ -33,9 +54,9 @@ public class MP3Player extends Application {
         stopButton.setOnAction(e -> mediaPlayer.stop());
 
         VBox vBox = new VBox(10);
-        vBox.getChildren().addAll(playButton, pauseButton, stopButton);
+        vBox.getChildren().addAll(selectFileButton, playButton, pauseButton, stopButton);
 
-        Scene scene = new Scene(vBox, 200, 100);
+        Scene scene = new Scene(vBox, 300, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
